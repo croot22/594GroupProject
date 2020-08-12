c56fe0fed9c6c56e969b6afa56de6b3aefdc444f
croot22 <croot22@seas.upenn.edu>
2020-08-11 18:27:40 -0600

    created zipCode class to implement something in data

---
+++ src/edu/upenn/cit594/data/ZipCode.java
@@ -0,0 +1,14 @@
+package edu.upenn.cit594.data;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ZipCode {
+	
+	public int zipCode;
+	public int population;
+	public LinkedList<Integer> fines = new LinkedList<Integer>();
+	public LinkedList<Integer> livableArea = new LinkedList<Integer>();
+	public LinkedList<Integer> marketValue = new LinkedList<Integer>();
+	
+}
