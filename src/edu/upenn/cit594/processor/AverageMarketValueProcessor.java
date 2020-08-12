6892742579a4bbabbabc614574dc660a0f3db3d8
croot22 <croot22@seas.upenn.edu>
2020-08-12 15:18:29 -0600

    Minor changes to improve strat design

--- src/edu/upenn/cit594/processor/AverageMarketValueProcessor.java
+++ src/edu/upenn/cit594/processor/AverageMarketValueProcessor.java
@@ -2,16 +2,13 @@ package edu.upenn.cit594.processor;
 
 import edu.upenn.cit594.data.OverallData;
 import edu.upenn.cit594.data.ZipCodeData;
-import edu.upenn.cit594.datamanagement.ReadProperties;
 
 public class AverageMarketValueProcessor implements AverageProcessor{
-	ZipCodeData zipData = new ZipCodeData();
-	ReadProperties rp = new ReadProperties();
-	ZipCodeProcessor zipProcessor = new ZipCodeProcessor();
+
 	
 	@Override
 	public double getAverage(String fileName, int zip) {
-		zipData = OverallData.zipCodeMap.get(zip);
+		ZipCodeData zipData = OverallData.zipCodeMap.get(zip);
 
 		if (zipData.averageMarketValue == 0) {
 			if (zipData.totalMarketValue == 0) {
--- src/edu/upenn/cit594/processor/AverageProcessor.java
+++ src/edu/upenn/cit594/processor/AverageProcessor.java
@@ -1,6 +1,10 @@
 package edu.upenn.cit594.processor;
 
+import edu.upenn.cit594.datamanagement.ReadProperties;
+
 public interface AverageProcessor {
 
+	ReadProperties rp = new ReadProperties();
+	ZipCodeProcessor zipProcessor = new ZipCodeProcessor();
 	public double getAverage(String fileName, int zip);
 }
