ddbd2f642d007d059a5eff12e082a3ab15e0fa9c
croot22 <croot22@seas.upenn.edu>
2020-08-12 10:34:34 -0600

    Converting to use ZipCodeData class

--- src/edu/upenn/cit594/datamanagement/ReadPopulationFile.java
+++ src/edu/upenn/cit594/datamanagement/ReadPopulationFile.java
@@ -5,59 +5,63 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import edu.upenn.cit594.data.ZipCodeData;
 import edu.upenn.cit594.logging.Logging;
 import edu.upenn.cit594.main.Main;
 
 public class ReadPopulationFile {
 
-	public HashMap<Integer, Integer> readPopulationFile(String populationFileName) {
+	public void readPopulationFile(String populationFileName) {
 		File populationList = new File(populationFileName);
 		Scanner scanner;
 		HashMap<Integer, Integer> populations = new HashMap<Integer, Integer>();
 		try {
 			scanner = new Scanner(populationList);
-		} catch (FileNotFoundException e) {
-			return populations;
-			}  
-		
-		/*
-		 * If text file does not exist or can not be read
-		 */
-		
-		if ((populationList.canRead()) && (populationList.exists()) == false) {
-			System.out.println("Error text file does not exist or can not be read");
-			System.exit(0);
-		}
-		
-		
-		Logging logger = Logging.getInstance();
-		String currentTime = logger.getCurrentTime();
-		String logMessage = currentTime +" "+ populationFileName;
-		logger.log(logMessage);
-		
-		String nextline;
-		
-		/* 
-		 * Create population list
-		 */
-		
-		while(scanner.hasNextLine()) {  
-			nextline = scanner.nextLine();  
-			String dataValue[] = nextline.split(" ");
-			if (dataValue[1] != null) {
-				int zipCode = Integer.parseInt(dataValue[0]); 			     
-			    int population = Integer.parseInt(dataValue[1]);
-				if (populations.containsKey(zipCode)) {							//@Cayde this sum totaling probably should be moved to processor
-					int totalpopulation = populations.get(zipCode) + population;
-					populations.put(zipCode, totalpopulation);
-				}
-				else {
-					populations.put(zipCode, population);
-				}	
+			
+			/*
+			 * If text file does not exist or can not be read
+			 */
+			
+			if ((populationList.canRead()) && (populationList.exists()) == false) {
+				System.out.println("Error text file does not exist or can not be read");
+				System.exit(0);
 			}
-		}	
-		scanner.close();
-		return populations;
+			
+			
+			Logging logger = Logging.getInstance();
+			String currentTime = logger.getCurrentTime();
+			String logMessage = currentTime +" "+ populationFileName;
+			logger.log(logMessage);
+			
+			String nextline;
+			
+			/* 
+			 * Create population list
+			 */
+			
+			while(scanner.hasNextLine()) {  
+				nextline = scanner.nextLine();  
+				String dataValue[] = nextline.split(" ");
+				if (dataValue[1] != null) {
+					int zipCode = Integer.parseInt(dataValue[0]); 			     
+				    int population = Integer.parseInt(dataValue[1]);
+				    ZipCodeData zip = new ZipCodeData();
+				    if (ZipCodeData.zipCodeMap.containsKey(zipCode)) {
+				    	zip = ZipCodeData.zipCodeMap.get(zipCode);
+				    }
+			    	zip.population = population;
+			    	ZipCodeData.zipCodeMap.put(zipCode, zip);
+				}
+			}	
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File with the given name was not found.");
+			System.exit(0);
+		}  
+		
+
+
+
 	}
 	
 }
