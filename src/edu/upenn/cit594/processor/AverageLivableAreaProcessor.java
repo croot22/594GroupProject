212d58acd0934415dd0cc1cc1417dff396b6589b
croot22 <croot22@seas.upenn.edu>
2020-08-13 17:36:37 -0600

    debugging exceptions

--- src/edu/upenn/cit594/processor/AverageLivableAreaProcessor.java
+++ src/edu/upenn/cit594/processor/AverageLivableAreaProcessor.java
@@ -4,22 +4,19 @@ import edu.upenn.cit594.data.OverallData;
 import edu.upenn.cit594.data.ZipCodeData;
 
 public class AverageLivableAreaProcessor implements AverageProcessor{
-	
+	ZipCodeData zipData = new ZipCodeData();
 	/*
 	 * method to get average for livable area
 	 */
 	@Override
 	public double getAverage(String fileName, int zip, int selectedOption) {
-		ZipCodeData zipData = OverallData.zipCodeMap.get(zip);
-		if(zipData == null) {
-			rp.readProperties(selectedOption, fileName, zip);
-			zipData.totalLivableArea = zipProcessor.totalLivableAreas(zip);
-			zipData.averageLivableArea = zipProcessor.averageValue(zipData.totalLivableArea, 
-					zipData.households);
+		if(OverallData.zipCodeMap.containsKey(zip)) {
+			ZipCodeData zipData = OverallData.zipCodeMap.get(zip);
 		}
+
 		if (zipData.averageLivableArea == 0) {
 			if (zipData.totalLivableArea == 0) {
-				if (zipData.livableArea == null) {
+				if (zipData.livableArea.isEmpty()) {
 					rp.readProperties(selectedOption, fileName, zip);
 				}	
 				zipData.totalLivableArea = zipProcessor.totalMarketValue(zip);
@@ -28,7 +25,7 @@ public class AverageLivableAreaProcessor implements AverageProcessor{
 			zipData.averageLivableArea = zipProcessor.averageValue(zipData.totalLivableArea, 
 					zipData.households);
 		}
-		
+
 		return zipData.averageMarketValue;
 	}
 
--- src/edu/upenn/cit594/processor/AverageMarketValueProcessor.java
+++ src/edu/upenn/cit594/processor/AverageMarketValueProcessor.java
@@ -4,22 +4,19 @@ import edu.upenn.cit594.data.OverallData;
 import edu.upenn.cit594.data.ZipCodeData;
 
 public class AverageMarketValueProcessor implements AverageProcessor{
-
+	ZipCodeData zipData = new ZipCodeData();
 	/*
 	 * method to calculate average for market value
 	 */
 	@Override
 	public double getAverage(String fileName, int zip, int selectedOption) {
-		ZipCodeData zipData = OverallData.zipCodeMap.get(zip);
-		if(zipData == null) {
-			rp.readProperties(selectedOption, fileName, zip);
-			zipData.totalMarketValue = zipProcessor.totalMarketValue(zip);
-			zipData.averageMarketValue = zipProcessor.averageValue(zipData.totalMarketValue, 
-					zipData.households);
+		if(OverallData.zipCodeMap.containsKey(zip)) {
+			zipData = OverallData.zipCodeMap.get(zip);
 		}
+
 		if (zipData.averageMarketValue == 0) {
 			if (zipData.totalMarketValue == 0) {
-				if (zipData.marketValue == null) {
+				if (zipData.marketValue.isEmpty()) {
 					rp.readProperties(selectedOption, fileName, zip);
 				}	
 				zipData.totalMarketValue = zipProcessor.totalMarketValue(zip);
